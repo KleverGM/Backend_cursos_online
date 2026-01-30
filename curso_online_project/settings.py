c3d101e48016e7ac7f989b82e2f7acf0513b9ca6
KleverGM <klevergeovannygm@hotmail.com>
2026-01-30 11:10:43 -0500

    conexion con frontend

--- curso_online_project/settings.py
+++ curso_online_project/settings.py
@@ -264,6 +264,9 @@ else:
         "http://127.0.0.1:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://courses-app.desarrollo-software.xyz",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
